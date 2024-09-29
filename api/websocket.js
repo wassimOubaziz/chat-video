173b8e73949afa5d05396302f80163d02bb96123
Wassim Oubaziz <wassimdjaafar5@gmail.com>
2024-09-29 11:00:19 +0100

    Refactor WebSocket handling and server setup

--- api/websocket.js
+++ api/websocket.js
@@ -10,7 +10,7 @@ module.exports = (req, res) => {
     wss.on("connection", (ws) => {
       ws.on("message", (message) => {
         wss.clients.forEach((client) => {
-          if (client !== ws && client.readyState === client.OPEN) {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
             client.send(message);
           }
         });
--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // index.js
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 const path = require("path");
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ noServer: true });
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
+
+if (require.main === module) {
+  server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+  });
+}
 
 module.exports = app;
