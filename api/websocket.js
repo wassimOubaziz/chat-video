7abe70cfdf9ba75ec68ad62b0ed84dbd8b61d043
Wassim Oubaziz <wassimdjaafar5@gmail.com>
2024-09-29 10:53:59 +0100

    Refactor static file serving and add WebSocket API

---
+++ api/websocket.js
@@ -0,0 +1,32 @@
+// api/websocket.js
+const { Server } = require("ws");
+
+let wss;
+
+module.exports = (req, res) => {
+  if (!wss) {
+    wss = new Server({ noServer: true });
+
+    wss.on("connection", (ws) => {
+      ws.on("message", (message) => {
+        wss.clients.forEach((client) => {
+          if (client !== ws && client.readyState === client.OPEN) {
+            client.send(message);
+          }
+        });
+      });
+
+      ws.on("close", () => {
+        console.log("Client disconnected");
+      });
+    });
+
+    res.socket.server.on("upgrade", (request, socket, head) => {
+      wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit("connection", ws, request);
+      });
+    });
+  }
+
+  res.end();
+};
--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 // index.js
 const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
+const path = require("path");
 
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-// Serve static files from the 'public' folder
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-// WebSocket connection handling
-wss.on("connection", (ws) => {
-  console.log("New client connected " + ws._socket.remoteAddress);
-
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
-    // Broadcast the message to all clients
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+module.exports = app;
